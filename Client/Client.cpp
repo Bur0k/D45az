597f9d5ebd98bc866df08dad2eecb14a9ffa4f95
Bur0k <ok.burak@web.de>
2013-12-11 16:56:13 +0100

    Client angefangen

---
+++ Client/Client.cpp
@@ -0,0 +1,54 @@
+#include "Client.h"
+
+Client::Client()
+{
+}
+
+void Client::connectToServer(string ip, int port)
+{
+	s=socket(AF_INET,SOCK_STREAM,0);
+
+	if(s==INVALID_SOCKET)
+	{
+		sendError(-1,"Socket could not be created: "+WSAGetLastError());
+		return ;
+	}
+
+	sockaddr_in si;
+	hostent *hostess_twinkies = gethostbyname(ip.c_str());
+
+	si.sin_family = AF_INET;
+	si.sin_port = htons(port);
+	si.sin_addr = *((in_addr*) hostess_twinkies->h_addr);
+	memset(&(si.sin_zero), 0, 8);
+	if (connect(s, (sockaddr*) &si, sizeof(struct sockaddr)) == -1)
+	{
+		sendError(-2,"Socket could not connect: "+WSAGetLastError());
+		return ;
+	}
+}
+
+
+void Client::sendError(int errCode,string errMessage)
+{
+	for(unsigned int i=0;i<errorCallback.size();i++)
+		errorCallback[i](errCode,errMessage);
+}
+
+void Client::sendNewMessage(short id,vector<char> data)
+{
+	for(unsigned int i=0;i<newMessageCallback.size();i++)
+		newMessageCallback[i](id,data);
+}
+
+void Client::write(short id, vector<char>data)
+{
+	vector<char> toSend(data);
+	short size = (short) data.size()+3;//2byte länge, 2byte id
+	
+	toSend.insert(toSend.begin(),id);
+}
+
+void Client::beginRead()
+{
+}
---
+++ Client/Client.h
@@ -0,0 +1,32 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+#pragma comment(lib, "Ws2_32.lib")
+
+class Client
+{
+	SOCKET s;
+
+	void sendError(int errCode,string errMessage);
+	void sendNewMessage(short id,vector<char> data);
+public:
+	vector<void (*)(int errCode,string errMessage)> errorCallback;
+	vector<void (*)(short id,vector<char> data)> newMessageCallback;
+
+	Client();
+
+	void connectToServer(string ip, int port);
+	void write(short id, vector<char>data);
+	void beginRead();
+};
+
+
+#endif
