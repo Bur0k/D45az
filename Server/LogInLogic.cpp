1075d20db85d3ef019f43a5aae958e8817af6924
FREAKII <stefanplail@gmx.de>
2013-12-16 16:44:44 +0100

    LogIn mit send / receive

--- Server/LogInLogic.cpp
+++ Server/LogInLogic.cpp
@@ -2,7 +2,7 @@
 
 LogInLogic::LogInLogic()
 {
-
+	//Message Callback Anmelden nicht vergessen!
 }
 
 LogInLogic::~LogInLogic()
@@ -11,36 +11,43 @@ LogInLogic::~LogInLogic()
 		delete connectedUsers[i];
 }
 
-void LogInLogic::connect()
+void LogInLogic::LogicMessageCallback(ClientHandler* ch,short id,vector<char> data)
 {
-	bool userexists = false;
-	short id;
-	vector<char> data;
-
-	if (id == 0x0100)
+	switch(id)
 	{
-		string name = "";
-
-		//testen
-		for (int i = 0; i < data.size(); i++) 
-			name += data[i];
-
-		for (int i = 0; i < connectedUsers.size(); i++)
+	case 0x0100:
 		{
-			if (connectedUsers[i]->getName()  == name)
-			{// send 0101 nachricht 0
-				userexists = true;
-				break;
+			bool userexists = false;
+			string name = "";
+
+			//testen
+			for (int i = 0; i < data.size(); i++) 
+				name += data[i];
+
+			for (int i = 0; i < connectedUsers.size(); i++)
+			{
+				if (connectedUsers[i]->getName()  == name)
+				{
+					// send 0101 nachricht 0
+					std::vector<char> erfg;
+					erfg.push_back(0);
+					ch->write(0x0101,erfg);
+
+					userexists = true;
+					break;
+				}
 			}
-		}
-		if (!userexists)
-		{
-			User* u = new User(name);
-			connectedUsers.push_back(u);
 
-			//send 0101 nachricht 1
-		}
+			if (!userexists)
+			{
+				User* u = new User(name);
+				connectedUsers.push_back(u);
 
+				//send 0101 nachricht 1
+				std::vector<char> erfg;
+					erfg.push_back(1);
+					ch->write(0x0101,erfg);
+			}
+		}break;
 	}
 }
-
