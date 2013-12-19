3019ecf5b3d41181f1c102577641e429319e1cd8
RichRat <ems.rir@gmail.com>
2013-12-19 23:17:11 +0100

    IDrawable, ausarbeitung von class Button

--- Client/Button.h
+++ Client/Button.h
@@ -2,16 +2,37 @@
 #define BUTTON_H
 
 #include <SFML\Graphics.hpp>
+#include <vector>
+#include "IDrawable.h"
 
 
-class Button
+typedef std::vector<void (*) (void)> FuncPointerVect;
+
+// implements basic button functionality
+// derived classes should implement visuals
+class Button : public IDrawable
 {
-private:
+protected:
 	sf::Vector2f size;
 	sf::Vector2f position;
+	FuncPointerVect attachedFunctions;
+	bool isEnabled;
+	//to avoid spamming attached functions
+	bool isClicked;
+
 public:
 	Button(sf::Vector2f size, sf::Color c);
 	~Button();
+
+	//implementing IDrawable
+	virtual void draw(sf::RenderWindow* rw);
+
+	bool isHit(sf::Vector2f);
+
+	//attaching a function pointer 
+	bool attachFunction(void (*pfunc)(void));
+	//detaching a function pointer
+	bool detachFunction(void (*pfunc)(void));
 
 };
 
