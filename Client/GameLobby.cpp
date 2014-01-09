85096ede354677c4ee994424b441ffbac6c6a095
FREAKII <stefanplail@gmx.de>
2014-01-09 12:25:40 +0100

    haaaand

--- Client/GameLobby.cpp
+++ Client/GameLobby.cpp
@@ -20,9 +20,25 @@ void GameLobby::processNewMessage(SOCKET s,short id,std::vector<char> data)
 		}break;
 	case 0x0302:
 		{
+			string name;
 
+			for(int i = 0; i > data.size(); i++)
+			{
+				while(data[i] != '/')
+					name += data[i];
+
+				this->players.push_back(name);
+			}
 		}break;
 	case 0x0304:
+		{
+
+		}break;
+	case 0x0305:
+		{
+
+		}break;
+	case 0x0306:
 		{
 
 		}break;
