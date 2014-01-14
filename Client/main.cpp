35f508aed83d63b1af9512a968897968f43fdb0c
Bur0k <ok.burak@web.de>
2014-01-14 10:46:10 +0100

    d

--- Client/main.cpp
+++ Client/main.cpp
@@ -117,7 +117,7 @@ int main()//Im Debug Mode verwenden wir Console als SubSystem. Es wird trotzdem 
 	}
 	g.onClose();
 	
-	//delete Client::get();
+	delete Client::get();
 
 	MyFonts::deleteFonts(); //TODO in game implementieren
 
