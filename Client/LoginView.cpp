0042dd830226146b7fb158b7c87af08404978f81
RichRat <ems.rir@gmail.com>
2014-01-15 12:56:08 +0100

    paskjflddfsfdsjoiiposdgöfhdsölkfjöald

--- Client/LoginView.cpp
+++ Client/LoginView.cpp
@@ -44,7 +44,9 @@ void LoginView::onTextBoxSend(int ID, std::string s)
 bool LoginView::isHit(sf::Vector2i & mouse)
 {
 	for(unsigned int i = 0; i < m_ClickV.size(); i++)
-		m_ClickV[i]->isHit(mouse);
+		if(m_ClickV[i]->isHit(mouse))
+			return true;
+	return false;
 }
 
 bool LoginView::PressedRight()
