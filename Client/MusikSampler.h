053aad8fd43a159cadf3999a7f9ace0f7acae60a
Mike00798 <hobbyimam@gmx.de>
2014-01-15 12:36:45 +0100

    Hintergrundmusik kann jetzt manuell weitergeschaltet werden (üblicher Button) später dann zeitlich abgefragt, ob altes lied zu ende > dann neues

--- Client/MusikSampler.h
+++ Client/MusikSampler.h
@@ -9,23 +9,30 @@ using namespace std;
 class MusikSampler
 {
 private:
-	sf::Music m_Music;	// nur OGG
+	sf::Music m_Music;	// OGG - immer nur 1 Song geladen
+	int m_Songnumber;
+
 	vector<sf::Sound*> m_vSound; // wird als Vector benötigt, um mehrere gleichzeitig zu starten	und anschließend zu deleten
 	vector<sf::SoundBuffer> m_vBuffer; // nur WAV
 
-	string m_Path_full_song;
-	string m_Path_sounds;
-	vector<string> m_vFull_songs;
-	vector<string> m_vShort_sounds;
+	string m_SongPath;
+	string m_Soundpath;
+	vector<string> m_vSongFiles;
+	vector<string> m_vSoundFiles;
 public:
+	//ctor & dtor
 	MusikSampler(void); 
 	~MusikSampler(void);
 
+	// lade und abspielfunktionen
     bool load_music(int index);		// im hintergrund ausgeführt
-	sf::SoundBuffer load_sound(int index); // schon am anfang ausgeführt
+	sf::SoundBuffer preload_soundbuffer(int index); // schon am anfang ausgeführt
     bool play_music(int index);
 	bool play_sound(int index);
+	void next_song();
 
+
+	// noch nicht verwendet
     void pause();
     void stop();
     int get_duration();
