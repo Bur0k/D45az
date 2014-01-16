2ceffa66f4ff34eba0a2b2eda3c80ce711a98c5e
FREAKII <stefanplail@gmx.de>
2014-01-16 12:28:50 +0100

    asd

--- Server/CityLogic.h
+++ Server/CityLogic.h
@@ -1,13 +1,18 @@
 #ifndef CITYLOGIC_H
 #define CITYLOGIC_H
 
+#include "NetworkParticipant.h"
+#include <vector>
+
 using namespace std;
 
-class CityLogic
+class CityLogic : public NetworkParticipant
 {
 private:
 
 public:
+	CityLogic();
+	~CityLogic();
 };
 
 #endif
