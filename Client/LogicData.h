c07d58d0f2ed7b2b4a1c50608e9a614bd978cdff
FREAKII <stefanplail@gmx.de>
2014-01-22 14:31:18 +0100

    commiterone

--- Client/LogicData.h
+++ Client/LogicData.h
@@ -1,18 +1,18 @@
-#ifndef GAMEDATA_H
-#define GAMEDATA_H
+#ifndef LOGICDATA_H
+#define LOGICDATA_H
 
 #include "NetworkParticipant.h"
 #include "Client.h"
 
-class GameData : public NetworkParticipant
+class LogicData : public NetworkParticipant
 {
 private:
 
 	Client* c;
 
 public:
-	GameData();
-	~GameData();
+	LogicData();
+	~LogicData();
 
 	void processNewMessage(short id,vector<char> data);
 	void processNetworkError(int id, std::string msg);
