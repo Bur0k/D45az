44ba0a559c05bdd808bad81239ea5b515ef9ae20
Bur0k <ok.burak@web.de>
2014-02-07 23:20:40 +0100

    aergvaegv

--- Client/IngameView.cpp
+++ Client/IngameView.cpp
@@ -574,14 +574,14 @@ void IngameView::moveMap()
 			m_enemy_armys[i]->m_mapViewOffset = Vector2i(m_mapView.left, m_mapView.top);
 	}
 	//update rectanglescity
-	if (tmpView != m_mapView)
+	/*if (tmpView != m_mapView)
 	{
 		for (unsigned int i = 0; i < m_GameData.allCities.size(); i++)
 	{
 			m_RectangleCityShapes[i].setPosition((float)(m_GameData.allCities[i]->position.x * m_tileSize.x - m_mapView.left + INGAMEVIEW_MOUSEOVER_RECT_BORDER),
 							(float)(m_GameData.allCities[i]->position.y * m_tileSize.y- m_mapView.top + INGAMEVIEW_MOUSEOVER_RECT_BORDER));
 		}
-	}
+	}*/
 
 
 	//update displayed armys
