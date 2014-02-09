6e6d36a250699bf2d7b3a74cd5fed173b4ca9e30
RichRat <ems.rir@gmail.com>
2014-02-09 23:08:51 +0100

    fuck fuck fuck fuck y

--- Client/IngameView.cpp
+++ Client/IngameView.cpp
@@ -863,6 +863,8 @@ void IngameView::loadGamestate()
 	m_owned_armys.clear();
 	m_enemy_armys.clear();
 	
+	updateFogOfWar();
+
 	short my_ID;
 	//my player id is read from the city at position 0
 	if(m_GameData.ownedCities.size() > 0)
@@ -877,7 +879,7 @@ void IngameView::loadGamestate()
 
 	for(unsigned int i = 0; i < m_GameData.allUnits.size(); i++)
 		if(m_GameData.allUnits[i]->player_ID != my_ID)
-			m_enemy_armys.push_back(new Army(m_GameData.allUnits[i], m_mapView, isVisible(Vector2i(m_GameData.allUnits[i]->pos.x, m_GameData.allUnits[i]->pos.x)),isInCity(Vector2i(m_GameData.allUnits[i]->pos.x, m_GameData.allUnits[i]->pos.y))));
+			m_enemy_armys.push_back(new Army(m_GameData.allUnits[i], m_mapView, isVisible(Vector2i(m_GameData.allUnits[i]->pos.x, m_GameData.allUnits[i]->pos.y)),isInCity(Vector2i(m_GameData.allUnits[i]->pos.x, m_GameData.allUnits[i]->pos.y))));
 
 	//clear RectangleVector
 	m_RectangleCityShapes.clear();
@@ -899,7 +901,6 @@ void IngameView::loadGamestate()
 		r.setSize(sf::Vector2f((float)(m_tileSize.x - 2 * INGAMEVIEW_MOUSEOVER_RECT_BORDER),(float)(m_tileSize.y- 2 * INGAMEVIEW_MOUSEOVER_RECT_BORDER)));
 		m_RectangleCityShapes.push_back(r);
 	}
-	updateFogOfWar();
 }
 
 bool IngameView::isInCity(Vector2i pos)
